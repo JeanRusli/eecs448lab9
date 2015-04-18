ae2b6ee4d16e97b4cf37abde412617ace84097b5
Jake Suddock <jacob.suddock@gmail.com>
2015-04-18 16:16:26 -0500

    Modification to Model and View classes

--- model.py
+++ model.py
@@ -1,12 +1,33 @@
 class Model:
-	def addValues(self, num1, num2):
-		return (num1 + num2)
+	def __init__(self, sol)
+		self.sol == 0.0
 	
-	def subtractValues(self, num1, num2):
-		return (num1 - num2)
+	def validateNumber(self, num): #Checks for valid number input
+		try:
+			returnNum = float(num)
+			return True
+		except ValueError: #Input was not a number so requests the number again
+			return False
 	
-	def multiplyValues(self, num1, num2):
-		return (num1 * num2)
+	def validateOperator(self, op): #Checks for valid operator input (+,-,*,/)
+		if op == "+" or op == "-" or op == "*" or op == "/":
+			return True
+		else:
+			return False
 	
-	def divideValues(self, num1, num2):
-		return (num1 / num2)
+	def isZero(self, num): #Checks for divide by zero
+		if num == 0:
+			return True
+		else:
+			return False
+	
+	def performOperation(self, num1, num2, op) #Returns solution to operation
+		if op == "+":
+			self.sol = float(num1) + float(num2)
+		elif op == "-":
+			self.sol = float(num1) - float(num2)
+		elif op == "*":
+			self.sol = float(num1) * float(num2)
+		else:
+			self.sol = float(num1) / float(num2)
+		return sol
--- view.py
+++ view.py
@@ -1,3 +1,23 @@
 class View:
-	def displayCalculator():
-		print
+	def welcomeMessage(self):
+		print "Welcome to the Python Calculator!\n"
+		print "I can add, subtract, multiply, and divide 2 numbers.\n\n"
+		print "Please enter your two numbers (enter \'ans\' to use the previous solution):"
+	
+	def inputNumber(self, text):
+		return raw_input("Input %s Number: " % text)
+	
+	def inputOperator(self):
+		return raw_input("Input Operator (+,-,*,/): ")
+	
+	def invalidNumber(self):
+		print "Either input a valid number or input \'ans\' without the quotes.\n"
+	
+	def invalidOperator(self):
+		print "Must enter either +, -, *, or /.\n"
+	
+	def printSolution(self, number1, number2, operator, solution)
+		print "%d %s %d =\n\t%d" % (number1, operator, number2, solution)
+	
+	def userContinue(self):
+		return raw_input("Would you like to continue? (y/N): ")
